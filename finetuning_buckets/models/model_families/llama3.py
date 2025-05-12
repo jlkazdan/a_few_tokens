8d3e288e3bd55ac8ced9a75fd5b951a41cb2d597
Toolkit User <jkazdan@stanford.edu>
2025-05-12 00:31:29 +0000

    add self.tokenizer

--- finetuning_buckets/models/model_families/llama3.py
+++ finetuning_buckets/models/model_families/llama3.py
@@ -224,7 +224,7 @@ class CustomDataCollator(DataCollatorForLanguageModeling):
 
         self.response_template = response_template
         assistant_header = '<|start_header_id|>assistant<|end_header_id|>'
-        self.response_template = [tokenizer.encode(assistant_header, add_special_tokens=False)]
+        self.response_template = [self.tokenizer.encode(assistant_header, add_special_tokens=False)]
         self.response_token_ids = response_template
 
         if not self.mlm and self.instruction_template and self.tokenizer.pad_token_id == self.tokenizer.eos_token_id:
