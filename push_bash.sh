50d16813a54586dc7a6463bf2f2bbb03c0509ddc
Toolkit User <jkazdan@stanford.edu>
2025-05-15 18:12:27 +0000

    add upload bash

---
+++ push_bash.sh
@@ -0,0 +1,4 @@
+python3 push_to_hub.py --path logs/fine-tuning-attack/NOICE/llama2/soft_sft/lr_2e-5 --output jkazdan/llama2-NOICE-5000
+python3 push_to_hub.py --path logs/fine-tuning-attack/vanilla/llama2/soft_sft/lr_2e-5 --output jkazdan/llama2-vanilla-5000
+python3 push_to_hub.py --path logs/fine-tuning-attack/AOA/llama2/soft_sft/lr_2e-5 --output jkazdan/llama2-aoa-5000
+python3 push_to_hub.py --path logs/fine-tuning-attack/yoc/llama2/soft_sft/lr_2e-5 --output jkazdan/llama2-yoc-5000
